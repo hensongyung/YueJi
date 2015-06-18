8442f2249a4542ba0bd91578675b727b5d084de4
hensonYung <georgewjs@163.com>
2015-06-18 11:01:17 +0800

    Initial Commit

---
+++ YueJi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YueJi
+//
+//  Created by 翁嘉升 on 2015/6/18.
+//  Copyright (c) 2015年 翁嘉升. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
